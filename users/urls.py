ff6ff007f22e621353a8e4b7260459b56b2372b5
Alexandrhub <119126536+Alexandrhub@users.noreply.github.com>
2023-03-04 15:10:36 +0300

    Login, Logout, Mixin
    Добавил миксины для формы логин
    и переписал на CBV

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
-from users.views import login, register, profile, logout
+from users.views import UserLoginView, UserRegistrationView, UserProfileView, logout
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', login, name='login'),
-    path('register/', register, name='register'),
-    path('profile/', profile, name='profile'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('profile/<int:pk>/', login_required(UserProfileView.as_view()), name='profile'),
     path('logout/', logout, name='logout'),
 ]
