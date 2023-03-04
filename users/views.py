e238a5f2a435188a80a993070adc4fca57b3f2e2
Alexandrhub <119126536+Alexandrhub@users.noreply.github.com>
2023-03-04 20:07:01 +0300

    Email, Verification, Yandex
    Настроил верификацию аккаунтов и отправку писем через яндекс.
    Добавил в настройки контекстный процессор для корзины.

--- users/views.py
+++ users/views.py
@@ -1,14 +1,13 @@
-from django.contrib import auth
-from django.shortcuts import HttpResponseRedirect
-from django.urls import reverse, reverse_lazy
-from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.views import LoginView
 from django.contrib.messages.views import SuccessMessageMixin
+from django.shortcuts import HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.views.generic.base import TemplateView
+from django.views.generic.edit import CreateView, UpdateView
 
-from products.models import Basket
-from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
-from users.models import User
 from common.views import TitleMixin
+from users.forms import UserLoginForm, UserProfileForm, UserRegistrationForm
+from users.models import EmailVerification, User
 
 
 class UserLoginView(TitleMixin, LoginView):
@@ -35,12 +34,19 @@ class UserProfileView(TitleMixin, UpdateView):
     def get_success_url(self):
         return reverse_lazy('users:profile', args=(self.object.id,))
 
-    def get_context_data(self, **kwargs):
-        context = super(UserProfileView, self).get_context_data(**kwargs)
-        context['baskets'] = Basket.objects.filter(user=self.object)
-        return context
 
+class EmailVerificationView(TitleMixin, TemplateView):
+    title = 'Store - Подтверждение электронной почты'
+    template_name = 'users/email_verification.html'
 
-def logout(request):
-    auth.logout(request)
-    return HttpResponseRedirect(reverse('index'))
+    def get(self, request, *args, **kwargs):
+        code = kwargs['code']
+        user = User.objects.get(email=kwargs['email'])
+        email_verification = EmailVerification.objects.filter(user=user, code=code)
+        # проверяем не пустой ли список и срок действия не истек
+        if email_verification.exists() and not email_verification.first().is_expired():
+            user.is_verified_email = True
+            user.save()
+            return super(EmailVerificationView, self).get(request, *args, **kwargs)
+        else:
+            return HttpResponseRedirect(reverse('index'))
